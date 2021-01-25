0014e49a27ab9386ffed92592e9592999b830e08
HuyenNguyen <huyenthingocnguyen@kms-technology.com>
2021-01-25 15:18:10 +0700

    Resolve UserHeader

--- src/components/header/components/UserHeader.js
+++ src/components/header/components/UserHeader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../stylesheets/userHeader.module.sass'
+import "../stylesheets/override.scss";
 import { NavDropdown } from 'react-bootstrap'
 import Auth from '../../../modules/Auth'
 import jumpTo from '../../../modules/Navigation'
@@ -7,6 +8,8 @@ import { faShoppingCart, faUser  } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Menu, MenuItem, anchorEl } from '@material-ui/core';
 import AccountDropDown from './AccountDropDown';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function UserHeader({ user_token }) {
   return (
     <div className={styles.outbox}>
@@ -15,9 +18,15 @@ export default function UserHeader({ user_token }) {
       </div>
       {(user_token && Object.keys(user_token).length > 0)
         ?
-        <AccountDropDown/>
-        : 
-        <div className={styles.loggout}>
+        <div>
+          <NavDropdown className={styles.tag} title={<FontAwesomeIcon icon={ faUser }/>}>
+            <NavDropdown.Item onClick={Auth.logout} href='/'>
+              Logout
+             </NavDropdown.Item>
+          </NavDropdown>
+        </div>
+        :
+        <div>
           <div className={styles.tag} onClick={() => jumpTo('/login')}>
             <FontAwesomeIcon icon={ faUser }/>
         </div>
