3861ca8397bd23214be07fed07bf093a4db455e9
HuyenNguyen <huyenthingocnguyen@kms-technology.com>
2021-02-28 00:09:43 +0700

    Redo search box

--- src/components/autoComplete/AutoComplete.js
+++ src/components/autoComplete/AutoComplete.js
@@ -1,85 +1,113 @@
 import React, { Component } from 'react'
-import styles from './auto.module.sass'
+import Autosuggest from 'react-autosuggest'
+import theme from './auto.module.sass'
+import jumpTo from '../../modules/Navigation'
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion}</span>
+  );
+}
+
+function renderSectionTitle(section) {
+  return (
+    <strong>{section.title}</strong>
+  );
+}
+
+function getSectionSuggestions(section) {
+  return section.suggestions;
+}
+
 export default class AutoComplete extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isFocus: false,
-      val: ''
-    }
-  }
-  handleChange = (e) => {
-    const input = e.target.value
-    this.props.filter(input)
-    if (this.props.onChange) {
-      this.props.onChange(input)
-    }
-    this.setState({
-      val: input
-    })
-  }
-  handleFocus = (e) => {
-    this.setState({
-      isFocus: true
-    })
-    this.handleChange(e)
-  }
-  handleBlur = () => {
-    let blurTimer=null
-    blurTimer=setTimeout(() => {
-      this.setState({
-        isFocus: false
-      },()=>clearTimeout(blurTimer))
-    }, 100);
-  }
-  handleClick = (v) => {
-    if (this.props.suggest_value) {
-      this.props.suggest_value(v)
-    }
-    this.setState({
-      val: v
-    }, () => {
-      this.props.search(v)
-    })
-  }
-  render() {
-    const r = this.props.filter_result;
 
+    this.state = {
+      value: '',
+      suggestions: []
+    };
+    
+    this.fetchFilterKeys()
+  }
+
+  fetchFilterKeys = () => {
+    this.props.filter('');
+    var searchKeys = this.props.filter_result;
+  }
+
+  getSuggestions = (value) => {
+    // this.props.filter(value);
+    if (this.props.onChange) {
+      this.props.onChange(value);
+    }
+    var searchKeys = this.props.filter_result;
+    const escapedValue = escapeRegexCharacters(value.trim());
+    
+    const regex = new RegExp(escapedValue, 'i');
+
+    return Object.keys(searchKeys ||{})
+      .map(section => {
+        return {
+          title: section.toUpperCase(),
+          suggestions: searchKeys[section].filter(key => regex.test(key))
+        };
+      })
+      .filter(filteredResult => filteredResult.suggestions.length > 0);
+  }
+  
+  getSuggestionValue = (suggestion) => {
+    return suggestion;
+  }
+
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    }, () =>{
+      if (newValue === '') {
+        jumpTo('/dashboard')
+      }
+      this.props.search(newValue)
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Search...",
+      value,
+      onChange: this.onChange
+    };
     return (
-      <div className={styles.outbox} style={this.props.style}>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          onFocus={this.handleFocus}
-          onBlur={this.handleBlur}
-          value={this.state.val}
-          placeholder="Search..."
-        />
-        <div className={styles.sugges_outbox}>
-          {
-            this.state.isFocus &&
-            !this.props.error && !this.props.loading &&
-            Object.keys(r || {}).map(f =>
-              <div key={f} className={styles.sugges_box}>
-                <div className={styles.sugges_category}>
-                  {f}
-                </div>
-                <div className={styles.sugges_arr}>
-                  {r[f].map(s =>
-                    <div
-                      key={s}
-                      className={styles.sugges_value}
-                      onClick={() => this.handleClick(s)}
-                    >
-                      {s}
-                    </div>
-                  )}
-                </div>
-              </div>
-            )
-          }
-        </div>
-      </div>
-    )
+        <Autosuggest 
+          theme={theme}
+          multiSection={true}
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={this.getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          renderSectionTitle={renderSectionTitle}
+          getSectionSuggestions={getSectionSuggestions}
+          inputProps={inputProps} />
+    );
   }
 }
